8fa85fbd31a748f1b0677bc68c0bcb7287caf701
amharrison <amharrison@gmail.com>
2013-04-29 15:50:37 -0400

    - extracted delegatedDeclarativeModule

--- org.jactr.modules.versioned/java/org/jactr/modules/versioned/declarative/DefaultVersionedDeclarativeModule.java
+++ org.jactr.modules.versioned/java/org/jactr/modules/versioned/declarative/DefaultVersionedDeclarativeModule.java
@@ -3,13 +3,8 @@ package org.jactr.modules.versioned.declarative;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 
 import javolution.util.FastList;
@@ -22,141 +17,31 @@ import org.jactr.core.chunk.IChunk;
 import org.jactr.core.chunktype.IChunkType;
 import org.jactr.core.model.IModel;
 import org.jactr.core.module.AbstractModule;
-import org.jactr.core.module.IllegalModuleStateException;
-import org.jactr.core.module.declarative.IDeclarativeModule;
-import org.jactr.core.module.declarative.associative.IAssociativeLinkageSystem;
 import org.jactr.core.module.declarative.basic.AbstractDeclarativeModule;
-import org.jactr.core.module.declarative.event.IDeclarativeModuleListener;
-import org.jactr.core.production.request.ChunkTypeRequest;
 import org.jactr.core.slot.ISlot;
 import org.jactr.core.utils.parameter.IParameterized;
-import org.jactr.core.utils.parameter.ParameterHandler;
+import org.jactr.modules.declarative.DelegatedDeclarativeModule;
 import org.jactr.modules.versioned.procedural.ProductionRewriter;
 
-public class DefaultVersionedDeclarativeModule implements
+public class DefaultVersionedDeclarativeModule extends DelegatedDeclarativeModule implements
     IVersionedDeclarativeModule, IParameterized
 {
 
-  static private final transient Log LOGGER                     = LogFactory
+  static final transient Log LOGGER                     = LogFactory
                                                                     .getLog(DefaultVersionedDeclarativeModule.class);
 
-  private IDeclarativeModule         _delegate;
-
-  private IModel                     _model;
-
-  private Map<String, String>        _deferredParameters;
-
-  static public final String         DECLARATIVE_DELEGATE_PARAM = "DeclarativeDelegateClass";
-
-  private ProductionRewriter         _rewriter;
+  ProductionRewriter         _rewriter;
 
   public DefaultVersionedDeclarativeModule()
   {
-    _deferredParameters = new TreeMap<String, String>();
+
   }
 
+  @Override
   public void install(IModel model)
   {
-    if (_delegate == null)
-      throw new IllegalModuleStateException(
-          "There must be a primary declarative module installed");
-
-    _model = model;
-
-    /*
-     * apply the deferred parameters
-     */
-    for (Map.Entry<String, String> param : _deferredParameters.entrySet())
-      if (_delegate instanceof IParameterized)
-        ((IParameterized) _delegate).setParameter(param.getKey(),
-            param.getValue());
-
-    _delegate.install(model);
-
+    super.install(model);
     _rewriter = new ProductionRewriter(_model);
-  }
-
-  public void initialize()
-  {
-    _delegate.initialize();
-  }
-
-  public String getName()
-  {
-    return _delegate.getName();
-  }
-
-  public IModel getModel()
-  {
-    return _model;
-  }
-
-  public void dispose()
-  {
-    _delegate.dispose();
-  }
-
-  public void reset()
-  {
-    _delegate.reset();
-  }
-
-  public void uninstall(IModel model)
-  {
-    _model = null;
-    _delegate.uninstall(model);
-  }
-
-  public Object getAdapter(Class adapterClass)
-  {
-    Object rtn = null;
-    if (adapterClass.isAssignableFrom(getClass())) rtn = this;
-
-    if (rtn == null) // check delegates
-      rtn = _delegate.getAdapter(adapterClass);
-
-    return rtn;
-  }
-
-  public void setParameter(String key, String value)
-  {
-    if (DECLARATIVE_DELEGATE_PARAM.equalsIgnoreCase(key))
-      try
-      {
-        _delegate = (IDeclarativeModule) ParameterHandler.classInstance()
-            .coerce(value).newInstance();
-      }
-      catch (Exception e)
-      {
-        LOGGER.error(
-            String.format("Could not create primary delegate %s %s", value,
-                e.getMessage()), e);
-        throw new IllegalModuleStateException(String.format(
-            "Could not create primary delegate %s %s", value, e.getMessage()));
-      }
-    else
-      _deferredParameters.put(key, value);
-  }
-
-  public String getParameter(String key)
-  {
-    if (key.equals(DECLARATIVE_DELEGATE_PARAM))
-      return _delegate.getClass().toString();
-    else
-      return ((IParameterized) _delegate).getParameter(key);
-  }
-
-  public Collection<String> getPossibleParameters()
-  {
-    return getSetableParameters();
-  }
-
-  public Collection<String> getSetableParameters()
-  {
-    Collection<String> params = ((IParameterized) _delegate)
-        .getSetableParameters();
-    params.add(DECLARATIVE_DELEGATE_PARAM);
-    return params;
   }
 
   public Future<IChunkType> getChunkType(final String name, final double version)
@@ -164,14 +49,17 @@ public class DefaultVersionedDeclarativeModule implements
     return AbstractModule.delayedFuture(new Callable<IChunkType>() {
       public IChunkType call() throws Exception
       {
-        LOGGER.debug("Requesting chunk type " + name + " and returning "
-            + getChunkTypeInternal(name, version));
-        return getChunkTypeInternal(name, version);
+        IChunkType ct = getChunkTypeInternal(name, version);
+
+        if (LOGGER.isDebugEnabled())
+          LOGGER
+              .debug("Requesting chunk type " + name + " and returning " + ct);
+        return ct;
       }
     }, ((AbstractDeclarativeModule) _delegate).getExecutor());
   }
 
-  private IChunkType getChunkTypeInternal(String name, double version)
+  protected IChunkType getChunkTypeInternal(String name, double version)
   {
     try
     {
@@ -204,6 +92,7 @@ public class DefaultVersionedDeclarativeModule implements
     return null;
   }
 
+
   public Future<IChunk> getChunk(final String name, final double version)
   {
     return AbstractModule.delayedFuture(new Callable<IChunk>() {
@@ -214,7 +103,7 @@ public class DefaultVersionedDeclarativeModule implements
     }, ((AbstractDeclarativeModule) _delegate).getExecutor());
   }
 
-  private IChunk getChunkInternal(String name, double version)
+  protected IChunk getChunkInternal(String name, double version)
   {
     try
     {
@@ -245,38 +134,6 @@ public class DefaultVersionedDeclarativeModule implements
       LOGGER.error(e);
     }
     return null;
-  }
-
-  public Future<IChunkType> createChunkType(IChunkType parent, String name)
-  {
-    return createChunkType(parent == null ? Collections.EMPTY_LIST
-        : Collections.singleton(parent), name);
-  }
-
-  public Future<IChunkType> createChunkType(Collection<IChunkType> parents,
-      String name)
-  {
-    return _delegate.createChunkType(parents, name);
-  }
-
-  public Future<IChunkType> addChunkType(IChunkType chunkType)
-  {
-    return _delegate.addChunkType(chunkType);
-  }
-
-  public Future<IChunkType> getChunkType(String name)
-  {
-    return getChunkType(name, _model.getAge());
-  }
-
-  public Future<Collection<IChunkType>> getChunkTypes()
-  {
-    return _delegate.getChunkTypes();
-  }
-
-  public Future<IChunk> createChunk(IChunkType parent, String name)
-  {
-    return _delegate.createChunk(parent, name);
   }
 
   public Future<IChunkType> refineChunkType(final IChunkType ct,
@@ -397,122 +254,6 @@ public class DefaultVersionedDeclarativeModule implements
     }
 
     return null;
-  }
-
-  public void dispose(IChunk chunk)
-  {
-    _delegate.dispose(chunk);
-  }
-
-  public IAssociativeLinkageSystem getAssociativeLinkageSystem()
-  {
-    return _delegate.getAssociativeLinkageSystem();
-  }
-
-  public void setAssociativeLinkageSystem(
-      IAssociativeLinkageSystem linkageSystem)
-  {
-    _delegate.setAssociativeLinkageSystem(linkageSystem);
-  }
-
-  public Future<IChunk> copyChunk(IChunk sourceChunk)
-  {
-    return _delegate.copyChunk(sourceChunk);
-  }
-
-  public Future<IChunk> copyChunk(IChunk sourceChunk, boolean copySubsymbolics)
-  {
-    return _delegate.copyChunk(sourceChunk, copySubsymbolics);
-  }
-
-  public Future<IChunk> addChunk(IChunk chunk)
-  {
-    return _delegate.addChunk(chunk);
-  }
-
-  public boolean willEncode(IChunk chunk)
-  {
-    return _delegate.willEncode(chunk);
-  }
-
-  public Future<IChunk> getChunk(String name)
-  {
-    return getChunk(name, _model.getAge());
-  }
-
-  public Future<Collection<IChunk>> getChunks()
-  {
-    return _delegate.getChunks();
-  }
-
-  public long getNumberOfChunks()
-  {
-    return _delegate.getNumberOfChunks();
-  }
-
-  public Future<Collection<IChunk>> findExactMatches(ChunkTypeRequest request,
-      Comparator<IChunk> sorter, double activationThreshold, boolean bestOne)
-  {
-    return _delegate.findExactMatches(request, sorter, activationThreshold,
-        bestOne);
-  }
-
-  public Future<Collection<IChunk>> findPartialMatches(
-      ChunkTypeRequest request, Comparator<IChunk> sorter,
-      double activationThreshold, boolean bestOne)
-  {
-    return _delegate.findPartialMatches(request, sorter, activationThreshold,
-        bestOne);
-  }
-
-  public IChunk getBusyChunk()
-  {
-    return _delegate.getBusyChunk();
-  }
-
-  public IChunk getEmptyChunk()
-  {
-    return _delegate.getEmptyChunk();
-  }
-
-  public IChunk getErrorChunk()
-  {
-    return _delegate.getErrorChunk();
-  }
-
-  public IChunk getFreeChunk()
-  {
-    return _delegate.getFreeChunk();
-  }
-
-  public IChunk getFullChunk()
-  {
-    return _delegate.getFullChunk();
-  }
-
-  public IChunk getNewChunk()
-  {
-    return _delegate.getNewChunk();
-  }
-
-  public IChunk getRequestedChunk()
-  {
-    return _delegate.getRequestedChunk();
-  }
-
-  public IChunk getUnrequestedChunk()
-  {
-    return _delegate.getUnrequestedChunk();
-  }
-
-  public void addListener(IDeclarativeModuleListener listener, Executor executor)
-  {
-    _delegate.addListener(listener, executor);
-  }
-
-  public void removeListener(IDeclarativeModuleListener listener)
-  {
-    _delegate.removeListener(listener);
   }
 
 }
